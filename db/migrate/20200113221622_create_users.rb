8aec168adcbf30458d2bbee20a2535c824c35fb0
Mark <m.hern1415@gmail.com>
2020-01-13 14:17:02 -0800

    user generated

---
+++ db/migrate/20200113221622_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.text :bio
+      t.string :image_url
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end

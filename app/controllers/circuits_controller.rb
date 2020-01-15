9936e53ca4e16336c0531442b0cb073e8fee0d44
Mark <m.hern1415@gmail.com>
2020-01-14 20:04:37 -0800

    fixed circuit views

--- app/controllers/circuits_controller.rb
+++ app/controllers/circuits_controller.rb
@@ -5,11 +5,24 @@ end
 
 def new
     @circuit = Circuit.new
+    @circuit.build_exercise
 end
 
 def create
+    @circuit = Circuit.new(circuit_params)
+    
+    if @circuit.save
+        redirect_to circuit_path(@circuit)
+    else
+        render :new
+    end
+    
 end
 
+private
 
+def circuit_params
+    params.require(:circuit).permit(:date, :category, :workout_id, :exercise_id, exercise_attributes:[:name, :category, :reps])
+end
 
 end
--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -2,5 +2,5 @@ class Circuit < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
   
-
+  accepts_nested_attributes_for :exercise 
 end

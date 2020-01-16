58461ba5d01258807693f88d2050a7c098a781ab
Mark <m.hern1415@gmail.com>
2020-01-16 11:28:33 -0800

    Changed Links and layout for circuits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   
     def create
       @user = User.find_by(username: params[:user][:username])
-      if @user && @user.authenticate(params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id 
         redirect_to user_path(@user)
       else

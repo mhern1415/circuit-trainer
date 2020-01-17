0dfadc52dad9ad72023c145726a87b08bc11ae86
Mark <m.hern1415@gmail.com>
2020-01-16 17:17:45 -0800

    added scopes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -5,7 +5,7 @@ class Workout < ApplicationRecord
 
     validates :title, :description, :circuit_count, presence: true
     
-
+    scope :three_biggest_workouts, -> { order(circuit_count: :desc).limit(3)}
 
     
 end

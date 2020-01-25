903dff79768d5f2da4d268b4a269938b5794cc2d
Mark <m.hern1415@gmail.com>
2020-01-24 17:33:33 -0800

    added validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,7 +3,7 @@ class Exercise < ApplicationRecord
   has_many :circuits
   has_many :workouts, through: :circuits 
 
-  validates :reps, presence: true
+  validates :reps, :category, :name, presence: true
 
   scope :most_used, -> { Exercise.left_joins(:circuits).group(:id).order("count(circuits.exercise_id) desc") }
 

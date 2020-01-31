59aafc7254ef91f96901869c134b0495a9b3535b
Mark <m.hern1415@gmail.com>
2020-01-31 15:20:53 -0800

    modified relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :created_exercises, foreign_key: "user_id", class_name: "Exercise"
     has_many :workouts
-    has_many :completed_exercises, through: :workouts, source: :exercises
+    has_many :exercises, through: :workouts
     has_many :circuits, through: :workouts
     has_secure_password
 
